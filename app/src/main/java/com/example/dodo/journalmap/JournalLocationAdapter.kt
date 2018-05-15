8a0fbd1d7bd28f94d2c5ca70cca9ae236b081efd
Do Hyung Kwon <thedavekwon@gmail.com>
2018-05-15 12:04:21 +0900

    05-15-2018 //JounralLocation ListView CameraMove

--- app/src/main/java/com/example/dodo/journalmap/JournalLocationAdapter.kt
+++ app/src/main/java/com/example/dodo/journalmap/JournalLocationAdapter.kt
@@ -2,12 +2,14 @@ package com.example.dodo.journalmap
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.gms.maps.model.LatLng
 import com.squareup.picasso.Picasso
 
 class JournalLocationAdapter(context: Context, resource: Int, objects: ArrayList<JournalLocation>) :
@@ -17,6 +19,11 @@ class JournalLocationAdapter(context: Context, resource: Int, objects: ArrayList
 
         if (view == null) {
             view = LayoutInflater.from(context).inflate(R.layout.activity_journal_list_view, parent, false)
+        }
+
+        view?.setOnClickListener {
+            (context as JournalActivity).moveMapCamera(LatLng(getItem(position).mLat, getItem(position).mLng))
+            Log.v("latlng", "latlng ${getItem(position).mLat}, ${getItem(position).mLng}")
         }
 
         val imageView = view?.findViewById<ImageView>(R.id.activity_journal_list_view_image)
