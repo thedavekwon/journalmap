3407466b32f15eacd0be6fbd86a3f7a0f76fc73a
DK <mysoon3765@gmail.com>
2018-05-17 20:47:20 +0900

    editor layout edit button bug

--- app/src/main/java/com/example/dodo/journalmap/EditorActivity.kt
+++ app/src/main/java/com/example/dodo/journalmap/EditorActivity.kt
@@ -1,22 +1,38 @@
 package com.example.dodo.journalmap
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.example.dodo.journalmap.R.id.*
 import io.objectbox.Box
 import io.objectbox.BoxStore
 import io.objectbox.annotation.Id
 import io.objectbox.kotlin.boxFor
 import kotlinx.android.synthetic.main.activity_editor.*
+import com.google.android.gms.*
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
 
-class EditorActivity : AppCompatActivity() {
+class EditorActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var journalLocationBox: Box<JournalLocation>
     private lateinit var journalLocation: JournalLocation
     private lateinit var mButton: Button
+    private lateinit var mMap: GoogleMap
+
+    private val EDITOR_CODE = 100
+
 
     private var mId = 0L
+
+    private val PLACE_PICKER_REQUEST = 101
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,19 +46,39 @@ class EditorActivity : AppCompatActivity() {
         // Get info from DB
         journalLocation = journalLocationBox.get(mId)
 
+        // Set up PlacePicker
+//        val intent = PlacePicker.IntentBuilder() as Intent
+
         // Set up edit button
         activity_editor_edit_button.setOnClickListener {
-            journalLocation.mText = activity_editor_Context.toString()
-            journalLocation.mName = activity_editor_Location.toString()
-            journalLocation.mDate = activity_editor_Date.toString()
+            journalLocation.mText = activity_editor_Context.editText?.text.toString()
+            journalLocation.mName = activity_editor_Name.editText?.text.toString()
+            journalLocation.mDate = activity_editor_Date.editText?.text.toString()
+
+            Log.v("location", "${journalLocation.mName}")
 
             journalLocationBox.put(journalLocation)
 
-            val intent = Intent(this, JournalActivity::class.java)
-            startActivity(intent)
-            finish()
+            setResult()
+
+
         }
     }
+
+    override fun onMapReady(googleMap : GoogleMap){
+        mMap = googleMap
+        var defaultposition = LatLng(20.0,20.0)
+        var default  = CameraPosition.builder().target(defaultposition).build() as CameraPosition
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(default))
+    }
+
+    fun setResult() {
+        val intent = Intent(this, JournalActivity::class.java)
+        setResult(Activity.RESULT_OK,intent)
+        finish()
+    }
+
+
 
 ////    fun updateJournalInfo{
 ////        journalLocation = journalLocationBox.get(mId)
