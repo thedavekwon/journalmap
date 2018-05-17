265ec54e39c520fc7b2bb498f46b19f070628db2
DK <mysoon3765@gmail.com>
2018-05-17 14:12:30 +0900

    editor activity changes

--- app/src/main/java/com/example/dodo/journalmap/EditorActivity.kt
+++ app/src/main/java/com/example/dodo/journalmap/EditorActivity.kt
@@ -2,11 +2,27 @@ package com.example.dodo.journalmap
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import io.objectbox.Box
+import io.objectbox.BoxStore
+import io.objectbox.annotation.Id
+import io.objectbox.kotlin.boxFor
 
 class EditorActivity : AppCompatActivity() {
+
+    private lateinit var journalLocationBox: Box<JournalLocation>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
+
+        // Set up for DB
+        journalLocationBox = (application as App).boxStore.boxFor<JournalLocation>()
+        mId = intent.getLongExtra("id",0)
+
+
+
+
     }
 }
